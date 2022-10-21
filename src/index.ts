4074634145c09b0129b2d0f68979367f2ce2c6c2
Jonathan Ferreira <j0n4t@hotmail.com>
2022-10-21 08:29:15 -0300

    fix: usar funções do script

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,15 @@ import { formatProjectMetadata } from "./formatProjectMetadata";
 import { getProjectMetadata } from "./getProjectMetadata";
 
 async function main() {
-  const data = await fs.readFile(path.resolve("data/raw.json"), "utf-8");
-  const result = JSON.parse(data);
+  // const data = await fs.readFile(path.resolve("data/raw.json"), "utf-8");
+  // const result = JSON.parse(data);
 
-  // const result = await getProjectMetadata({
-  //   org: process.env.ORGANIZATION || "",
-  //   projectNumber: process.env.PROJECT_NUMBER || "",
-  //   fieldsQty: Number(process.env.FIELDS_QUANTITY) || 1,
-  //   itemsQty: Number(process.env.ITEMS_QUANTITY) || 1,
-  // });
+  const result = await getProjectMetadata({
+    org: process.env.ORGANIZATION || "",
+    projectNumber: process.env.PROJECT_NUMBER || "",
+    fieldsQty: Number(process.env.FIELDS_QUANTITY) || 1,
+    itemsQty: Number(process.env.ITEMS_QUANTITY) || 1,
+  });
 
   await fs.writeFile(path.resolve("data/raw.json"), JSON.stringify(result));
 
